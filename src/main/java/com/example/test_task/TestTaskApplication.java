c7c3de17af4878dfdac2588198693a74a6a2bed0
AndriyBarskyi <barskyiandriy@gmail.com>
2023-08-21 15:27:39 +0300

    init project

---
+++ src/main/java/com/example/test_task/TestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskApplication.class, args);
+	}
+
+}
