83759ce44cae95355b9fa8d53684ad035842d4d3
AndriyBarskyi <barskyiandriy@gmail.com>
2023-08-22 14:38:13 +0300

    remove typo

--- src/main/java/com/example/test_task/controller/NumberController.java
+++ src/main/java/com/example/test_task/controller/NumberController.java
@@ -1,6 +1,5 @@
 package com.example.test_task.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.test_task.dto.NumberDTO;
@@ -17,7 +16,7 @@ public class NumberController {
 
     @PostMapping
     public void saveNumber(@RequestBody NumberDTO numberDto) {
-        numberService.saveNumber(numberDto););
+        numberService.saveNumber(numberDto);
     }
 
     @GetMapping
